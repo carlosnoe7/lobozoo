dccef5d1fc1e3688b190a3cb93a20ef9a7908744
noe <csalazar089@gmail.com>
2021-11-20 19:40:26 -0600

    Cambio en la imagen del mapa

--- lib/src/screens/mapascreen.dart
+++ lib/src/screens/mapascreen.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:lobozoo/src/widgets/buttonimg.dart';
 
-List<String> animals=['leon','mono','jirafa'];
-List<String> textosanimals=["Este es el texto del leon","Este es el texto del mono","Este es el texto de la jirafa"];
+//List<String>  animals = ['leon','mono','jirafa','jirafa']..length = 20;
+
+
+
+
+var animals=['leon','mono','jirafa','jirafa'];
+var textosanimals=["Este es el texto del leon","Este es el texto del mono","Este es el texto de la jirafa","Texto de jirafa"];
 
 
 class MapaScreen extends StatelessWidget {
@@ -17,19 +22,25 @@ class MapaScreen extends StatelessWidget {
        children: <Widget>[
          new Container(
            decoration: new BoxDecoration(
-             image:new DecorationImage(image: new AssetImage("assets/mapa.jpg"),fit: BoxFit.cover)
+             image:new DecorationImage(image: new AssetImage("assets/mapa2.png"),fit: BoxFit.cover)
            ),
          ),
          Center(
            child: StaggeredGridView.count(crossAxisCount: 4,
-             children: List.generate(3,
-                 (index)=>Center(
-                   child: ButtonImg(descripcion: textosanimals[index],img: "assets/"+animals[index]+".jpg")
-                 )),
+             children: <Widget>[
+               ButtonImg(descripcion: textosanimals[0],img: "assets/"+animals[0]+".jpg"),
+               ButtonImg(descripcion: textosanimals[1],img: "assets/"+animals[1]+".jpg"),
+               ButtonImg(descripcion: textosanimals[2],img: "assets/"+animals[2]+".jpg"),
+               ButtonImg(descripcion: textosanimals[2],img: "assets/"+animals[3]+".jpg"),
+               ButtonImg(descripcion: textosanimals[2],img: "assets/"+animals[3]+".jpg"),
+             ],
              staggeredTiles: const [
                StaggeredTile.count(2, 2), // takes up 2 rows and 2 columns space
                StaggeredTile.count(2, 1), // takes up 2 rows and 1 column
-               StaggeredTile.count(1, 2), // takes up 1 row and 2 column space
+               StaggeredTile.count(1, 2),
+               StaggeredTile.count(1, 2),
+               StaggeredTile.count(2, 2),
+               // takes up 1 row and 2 column space
              ],
            )
          )
@@ -38,3 +49,6 @@ class MapaScreen extends StatelessWidget {
     );
   }
 }
+/*
+ButtonImg(descripcion: textosanimals[index],img: "assets/"+animals[index]+".jpg")
+ */
