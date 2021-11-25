fb13f700ca652805bd4b6dd0f1173a58dadb2af3
noe <csalazar089@gmail.com>
2021-11-25 13:14:56 -0600

    Agregando un loading al mapa

--- lib/src/screens/mapascreen.dart
+++ lib/src/screens/mapascreen.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:lobozoo/src/widgets/buttonimg.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 //List<String>  animals = ['leon','mono','jirafa','jirafa']..length = 20;
 
 
 
 
 List<dynamic> animals=[];
-List<dynamic> nombresAnimales=["","",""];
-var textosanimals=["Este es el texto del leon","Este es el texto del mono","Este es el texto de la jirafa","Texto de jirafa"];
+List<dynamic> nombresAnimales=[];
+List<dynamic> textosanimals=[];
 
 Future<List<dynamic>> someFutureStringFunction() async {
   int i=0;
@@ -22,12 +23,15 @@ Future<List<dynamic>> someFutureStringFunction() async {
      // print(doc.data());
       //print(doc["name"]);
       print(doc["image"]);
-
+      //print("El indice es "+'$i');
       //animals[i]=doc["image"];
+      textosanimals.add(doc["description"]);
       animals.add(doc["image"]);
-      nombresAnimales[i]=  doc["name"];
+      //nombresAnimales[i]=  doc["name"];
+      nombresAnimales.add(doc["name"]);
       i++;
     });
+    i=0;
     return nombresAnimales;
   }).catchError((onError)=>print(onError));
 
@@ -60,8 +64,11 @@ class MapaScreen extends StatelessWidget {
                        ButtonImg(descripcion: textosanimals[0],img: animals[0],title:nombresAnimales[0]),
                        ButtonImg(descripcion: textosanimals[1],img: animals[1],title:nombresAnimales[1]),
                        ButtonImg(descripcion: textosanimals[2],img: animals[2],title:nombresAnimales[2]),
-                       ButtonImg(descripcion: textosanimals[1],img: animals[1],title:nombresAnimales[1]),
-                       ButtonImg(descripcion: textosanimals[2],img: animals[2],title:nombresAnimales[2]),
+                       ButtonImg(descripcion: textosanimals[3],img: animals[3],title:nombresAnimales[3]),
+                       ButtonImg(descripcion: textosanimals[4],img: animals[4],title:nombresAnimales[4]),
+                       ButtonImg(descripcion: textosanimals[5],img: animals[5],title:nombresAnimales[5]),
+                       ButtonImg(descripcion: textosanimals[6],img: animals[6],title:nombresAnimales[6]),
+                       ButtonImg(descripcion: textosanimals[7],img: animals[7],title:nombresAnimales[7]),
                      ],
                      staggeredTiles: const [
                        StaggeredTile.count(2, 2), // takes up 2 rows and 2 columns space
@@ -69,6 +76,9 @@ class MapaScreen extends StatelessWidget {
                        StaggeredTile.count(1, 2),
                        StaggeredTile.count(1, 2),
                        StaggeredTile.count(2, 2),
+                       StaggeredTile.count(2, 1), // takes up 2 rows and 1 column
+                       StaggeredTile.count(1, 2),
+                       StaggeredTile.count(2, 1), // takes up 2 rows and 1 column
                        // takes up 1 row and 2 column space
                      ],
                    )
@@ -76,7 +86,14 @@ class MapaScreen extends StatelessWidget {
              ],
            );
          } else {
-           return Text('Loading...');
+           return Scaffold(
+             body: Center(
+                child: SpinKitRotatingCircle(
+                  color: Colors.lightGreen,
+                  size: 50.0,
+                ),
+             ),
+           );
          }
        },
      )
